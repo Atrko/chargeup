ffaf7fc9474a8f93f326864ea6f0b099b2d69c07
Dallas Pataska <pataskad@gmail.com>
2020-11-25 15:23:04 -0600

    DP First Commit

---
+++ login/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import *
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    errors = User.objects.validate(request.POST)
+    if errors:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    else:
+        user = User.objects.create(
+            first_name = request.POST['first_name'],
+            last_name = request.POST['last_name'],
+            email = request.POST['email'],
+            password = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt()).decode()
+        )
+        request.session['user_id'] = user.id
+        return redirect('/home')
+
+def login(request):
+    if request.method == "GET":
+        return redirect('/')
+    if not User.objects.authenticate(request.POST['email'], request.POST['password']):
+        messages.error(request, 'Invalid Email/Password')
+        return redirect('/')
+    user = User.objects.get(email=request.POST['email'])
+    request.session['user_id'] = user.id
+    return redirect('/home')
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+def home(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, 'home.html', context)
+
+def profile(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'user': User.objects.get(id=request.session['user_id'])
+    }
+    return render(request, 'profile.html' , context)
